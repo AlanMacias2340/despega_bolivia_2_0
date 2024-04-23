204c1350d79db9202734e3e8bea3692e42112905
Alan J Macias <alanmacias2340@gmail.com>
2024-04-23 15:06:06 -0400

    Empezando de nuevo

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+/* ARCHIVO DE CONFIGURACION DE TAILWIND */
+module.exports = {
+  content: ["./src/**/*.{html,js}" , "./index.html"], // Agregamos el index.html para que lo lea tailwind
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
