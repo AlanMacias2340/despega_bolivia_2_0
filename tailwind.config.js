be0c9b8fd14f566fb713852e22dfee501af15b2a
Kevin Uruchi <uruchicoarite@gmail.com>
2024-04-26 15:19:25 -0400

    services added

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,27 @@
 module.exports = {
    content: ["./src/**/*.{html,js}", "./index.html"], // Agregamos el index.html para que lo lea tailwind
    theme: {
-      extend: {},
+      extend: {
+         animation: {
+           blob: 'blob 7s infinite'
+         },
+         keyframes: {
+           blob: {
+             '0%': {
+               transform: 'translate(0px, 0px) scale(1)'
+             },
+             '33%': {
+               transform: 'translate(30px, -50px) scale(1.1)'
+             },
+             '66%': {
+               transform: 'translate(20px, -20px) scale(0.9)'
+             },
+             '100%': {
+               transform: 'translate(0px, 0px) scale(1)'
+             },
+           }
+         }
+       },
       /* Medidas Responsive Personalizadas */
       /* screens: {
       'celular-small': {'min': '320px', 'max': '400px'},
